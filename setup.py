d79029d1d13b7345c329d0529495765ad9e8949d
HellOwhatAs <xjq701229@outlook.com>
2023-01-31 16:51:16 +0800

    v0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = 'KeyWeighted',
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'add option arg key to some lib',
     license = 'MIT License',
     install_requires = [],
